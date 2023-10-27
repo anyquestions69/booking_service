8c4e5fcebe70a1e8ccc99fa70f68bef3887f72ed
anyquestions69 <danil_tyrenko@icloud.com>
2023-10-27 10:28:53 +0300

    added email notification

--- app/controllers/seatController.js
+++ app/controllers/seatController.js
@@ -1,5 +1,16 @@
 const {Seat, Event} = require('../models/user')
 const { Op } = require("sequelize");
+const { createTransport } = require('nodemailer');
+const transporter = createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
+      // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+      user: "juuzodes@yandex.ru",
+      pass: "WFAMwSEbBOpKz9P4",
+    },
+  });
 function filterInput(data){
     for(let prop in data){
         console.log(prop)
@@ -68,6 +79,21 @@ class Manager{
                 return res.status(400).send({error:'Место уже занято'})
             }
             let response = await Seat.update({statusId:3, email:seat.email },{where:{id}})
+            const mailOptions = {
+                from: 'juuzodes@yandex.ru',
+                to: seat.email,
+                subject: `Бронь места`,
+                text: `<h4>Ваше место успешно забронировано!</h4>
+                        <p>Сектор: ${sector} Место: ${col} Ряд: ${row}</p>`
+            };
+            
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
             return res.send({res:"Место успешно забронировано"})
         }catch(e){
             console.log(e)
@@ -89,6 +115,38 @@ class Manager{
                 return res.status(400).send({error:'Место уже занято'})
             }
             let response = await Seat.update({statusId:2, email },{where:{active:true, row, col, sectorId}})
+            let sector = ''
+            switch (sectorId) {
+              case 1:
+                sector =  'Bronze'
+                break;
+              case 2:
+                sector =  'Silver'
+                break;
+              case 3:
+              sector =  'Gold'
+                break;
+              case 4:
+              sector =  'Platinum'
+                break;
+              default:
+                break;
+            }
+            const mailOptions = {
+                from: 'juuzodes@yandex.ru',
+                to: 'dan.tyrenko@yandex.ru',//process.env.ADMIN_EMAIL,
+                subject: `Новая заявка на бронирование`,
+                text: `<p>Пользователь <a href="mailto:${email}">${email}</a> оставил заявку на бронь</p>
+                        <p>Сектор: ${sector} Место: ${col} Ряд: ${row}</p>`
+            };
+            
+            transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                    console.log(error);
+                } else {
+                    console.log('Email sent: ' + info.response);
+                }
+            });
             return res.send({res:"Ожидайте рассмотрения заявки менеджером"})
         }catch(e){
             console.log(e)
