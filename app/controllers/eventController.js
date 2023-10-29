5d88b2784197ae043eec0f044b46b91702e139df
Public Hysteria <public_hysteria@uwu.domain.com>
2023-10-30 00:03:01 +0300

    reworked event creation

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -26,16 +26,16 @@ function createSeats(body, eventId){
         if(i<11){
             arenaSeats.push({row:4, col:i, statusId:1, sectorId:2, price:silver, eventId,active:true }) //4
         }
-        if(i>6 || 31){
+        if(i>6 && i<31){
             arenaSeats.push({row:5, col:i, statusId:1, sectorId:2, price:silver, eventId,active:true }) //5
         }
-        if(i>7 ||i<21 || i>26 || i<35){
+        if((i>7 && i<21) || (i>26 && i<35)){
             arenaSeats.push({row:6, col:i, statusId:1, sectorId:2, price:silver, eventId,active:true }) //6
         }
-        if(i>8 || i<43){
+        if(i>8 && i<43){
             arenaSeats.push({row:7, col:i, statusId:1, sectorId:2, price:silver, eventId,active:true }) //7
         }
-        if(i>20 || i<36){
+        if(i>20 && i<36){
             arenaSeats.push({row:8, col:i, statusId:1, sectorId:2, price:silver, eventId,active:true }) //8
         }
     }
@@ -50,9 +50,108 @@ function createSeats(body, eventId){
             arenaSeats.push({row:11, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //11
             arenaSeats.push({row:12, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //12
         }
-        if(i>32 || i<38)
-            arenaSeats.push({row:5, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //8
-       
+        if(i>32 && i<38)
+            arenaSeats.push({row:5, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //5
+        if(i>37 && i<42){
+            arenaSeats.push({row:6, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+        }
+        if(i>46 && i<51){
+            arenaSeats.push({row:7, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+        }
+        if(i>51 && i<56){
+            arenaSeats.push({row:8, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+        }
+        if(i>56 && i<61){
+            arenaSeats.push({row:11, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+            arenaSeats.push({row:9, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+        }
+        if(i>59 && i<64){
+            arenaSeats.push({row:12, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+            arenaSeats.push({row:10, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+        }
+        if(i<11){
+            arenaSeats.push({row:13, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+            arenaSeats.push({row:14, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+            arenaSeats.push({row:15, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+            arenaSeats.push({row:16, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true}) //6
+        }
+        if(i>69 && i<79){
+            arenaSeats.push({row:13, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true})
+        }
+        if(i>87 && i<95){
+            arenaSeats.push({row:14, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true})
+        }
+        if(i>84 && i<92){
+            arenaSeats.push({row:15, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true})
+        }
+        if(i>92 && i<102){
+            arenaSeats.push({row:16, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true})
+        }
+        if(i<63){
+            arenaSeats.push({row:18, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true})
+        }
+        if(i!=26 && i!=27 && i!= 78 && i!=79){
+            arenaSeats.push({row:17, col:i, statusId:1, sectorId:1, price:bronze, eventId,active:true})
+        }
+    }
+    for(let i=5;i<92;i++){
+        if(i<7||(i>30&&i<33)){
+            arenaSeats.push({row:5, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i<8||(i>34&&i<38)){
+            arenaSeats.push({row:6, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i<9||(i>42&&i<47)){
+            arenaSeats.push({row:7, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i<9||(i>47&&i<52)){
+            arenaSeats.push({row:8, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i<10||(i>51&&i<57)){
+            arenaSeats.push({row:9, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i<10||(i>54&&i<60)){
+            arenaSeats.push({row:10, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i<11||(i>50&&i<57)){
+            arenaSeats.push({row:11, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i<11||(i>53&&i<60)){
+            arenaSeats.push({row:12, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if((i>10&&i<25)||(i>55&&i<70)){
+            arenaSeats.push({row:13, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i>10&&i<87){
+            arenaSeats.push({row:14, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i>10&&i<83){
+            arenaSeats.push({row:15, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        if(i>10&&i<93&&i!=26&&i!=77){
+            arenaSeats.push({row:16, col:i, statusId:1, sectorId:4, price:platinum, eventId,active:true})
+        }
+        
+    }
+    for(let i=9;i<55;i++){
+        if(i<21||(i>35&&i<48)){
+            arenaSeats.push({row:8, col:i, statusId:1, sectorId:3, price:gold, eventId,active:true})
+        }
+        if(i<35||i>38){
+            arenaSeats.push({row:9, col:i, statusId:1, sectorId:3, price:gold, eventId,active:true})
+        }
+        if(i>9&&i<55){
+            arenaSeats.push({row:10, col:i, statusId:1, sectorId:3, price:gold, eventId,active:true})
+        }
+        if(i>10&&i<51&&i!=30&&i!=31){
+            arenaSeats.push({row:11, col:i, statusId:1, sectorId:3, price:gold, eventId,active:true})
+        }
+        if(i>10&&i<54&&i!=32&&i!=33){
+            arenaSeats.push({row:12, col:i, statusId:1, sectorId:3, price:gold, eventId,active:true})
+        }
+        if(i>24&&i<56&&i!=40&&i!=41){
+            arenaSeats.push({row:13, col:i, statusId:1, sectorId:3, price:gold, eventId,active:true})
+        }
     }
     return arenaSeats
    
