bbffc54996fd559d153847413ddf7ccc337b3047
Public Hysteria <public_hysteria@uwu.domain.com>
2023-10-30 23:52:04 +0300

    fixed front

--- static/admin/js/requests.js
+++ static/admin/js/requests.js
@@ -48,17 +48,20 @@ function show(){
           
             $('#reqList').append(`
             <div class="list-group-item list-group-item-action d-md-flex justify-content-between gap-2 py-3 align-items-center" aria-current="true">
-            
+            <div class="d-lg-flex gap-4 justify-content-start align-items-center">
              <h6 class="mb-0"><span>${i}. </span>${r.email}</h6>
             
-            <div class="d-lg-flex gap-2 justify-content-end align-items-center">
-            <div><span style="color:${color}">${sector}</span> Ряд: ${r.row} Место: ${r.col}</div>
+            
+            <div><div style="color:${color}">${sector}</div> Ряд: ${r.row} Место: ${r.col}</div>
             <div class="opacity-50 text-nowrap">${day}.${month}.${year}</div>
-                <div data-id=${r.id} 
-                class="mb-0  request-accept btn-success text-white btn" >Забронировать</div>
-                <div data-id=${r.id} class="mb-0 btn-danger text-small btn text-white request-decline ">Отклонить</div>
-              
             </div>
+            <div class="d-sm-dlex gap-1 justify-content-end align-items-center">
+            
+                <div data-id=${r.id} 
+                class="mb-0 p-2 request-accept btn-success text-white btn" >Забронировать</div>
+                <div data-id=${r.id} class=" p-2 mb-0 btn-danger text-small btn text-white request-decline ">Отклонить</div>
+              
+           
           </div>
             `)
             i++
