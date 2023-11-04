c78f59655c47942514e9033e64b20fa1bb731737
Public Hysteria <public_hysteria@uwu.domain.com>
2023-11-04 21:28:18 +0300

    mail

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -6,7 +6,6 @@ const { createTransport } = require('nodemailer');
 const transporter = createTransport({
     host: process.env.SMTP_SERVER,
     port:587,
-    secure: false,
     auth: {
      
       user: process.env.SMTP_MAIL,
